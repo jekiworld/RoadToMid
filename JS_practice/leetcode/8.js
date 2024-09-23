f5b59d3862d9b9f2fdab5bf0078249869c79f68b
Bakhredin <bax.2002@mail.ru>
2024-09-24 01:25:17 +0500

    leetcode contain dublicate with hashmap

--- JS_practice/leetcode/8.js
+++ JS_practice/leetcode/8.js
@@ -1,16 +1,32 @@
-var containsDuplicate = function (nums) {
-    const a = new Set(nums);
-    if(a.size !== nums.length){
-        console.log("true")
-        console.log(a);
-        console.log(nums);
-        console.log(a.size);
-        console.log(nums.length);
+// var containsDuplicate = function (nums) {
+//     const a = new Set(nums);
+//     if(a.size !== nums.length){
+//         console.log("true")
+//         console.log(a);
+//         console.log(nums);
+//         console.log(a.size);
+//         console.log(nums.length);
 
-        return true;        
+//         return true;        
+//     }
+//     console.log("fasle")
+//     return false;
+// };
+
+
+var containsDuplicate = function(nums) {
+ const table = {};
+ for(const i of nums){
+    if(table[i]){
+     
+        return true;
+    } else {
+        table[i] = true;
     }
-    console.log("fasle")
-    return false;
-};
+ }
+ console.log("false");
 
-containsDuplicate([1,2,3,4]);
+ return false;
+}
+
+containsDuplicate([1,2,3,1]);
