1846900818dbad7930cbb181b958571d1dcb7ad4
Bakhredin <bax.2002@mail.ru>
2024-11-28 11:06:22 +0500

    stack

---
+++ JS_practice/leetcode/stakc/removeStars.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++){
+        if(s[i]!=="*"){
+            stack.push(s[i]);
+        }
+        else{
+            stack.pop();
+        }
+    }
+    console.log(stack.join(""));
+
+};
+
+removeStars("erase***")
